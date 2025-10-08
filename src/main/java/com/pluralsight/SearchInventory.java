7630ff27578306011f381e3bae4f4c977b165ffd
KyrraRose <krbosland1@gmail.com>
2025-10-08 09:55:07 -0400

    Initial Commit

---
+++ src/main/java/com/pluralsight/SearchInventory.java
@@ -0,0 +1,12 @@
+package com.pluralsight;
+
+import java.util.ArrayList;
+
+public class SearchInventory {
+    public static void main(String[] args) {
+
+        ArrayList<String> inventory =new ArrayList<String>();
+
+
+    }
+}

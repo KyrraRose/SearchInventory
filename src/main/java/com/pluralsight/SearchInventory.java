59b157dd54c200d0c85dde08f17f062318cf1c0c
KyrraRose <krbosland1@gmail.com>
2025-10-08 10:06:30 -0400

    We can print our humble inventory

--- src/main/java/com/pluralsight/SearchInventory.java
+++ src/main/java/com/pluralsight/SearchInventory.java
@@ -4,9 +4,21 @@ import java.util.ArrayList;
 
 public class SearchInventory {
     public static void main(String[] args) {
+        ArrayList<String> inventory = getInventory();
+        for (int i = 0; i < inventory.size(); i++){
+            System.out.println(inventory.get(i));
+        }
 
+
+
+
+    }
+    public static ArrayList<String> getInventory(){
         ArrayList<String> inventory =new ArrayList<String>();
-
+        inventory.add("4567|10' 2x4 (grade B)|9.99");
+        inventory.add("1234|Hammer|19.49");
+        inventory.add("2345|Box of nails|9.29");
+        return inventory;
 
     }
 }
